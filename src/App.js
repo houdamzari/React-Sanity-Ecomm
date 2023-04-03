e40d66fd9fe1ddcec337b9f1ebcb05e446fce632
houdamzari <houda.mzari1999@gmail.com>
2023-04-03 10:08:57 +0000

    Setup react router

--- src/App.js
+++ src/App.js
@@ -2,33 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect } from "react";
 import { client } from './client'
+import Homepage from "./components/Homepage/Homepage";
+import { Routes, Route } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+
 function App() {
   const fetchdata = async () => {
       const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
-    console.log(products)
+    const products = await client.fetch(query);
+    const product = products.map(product => product.body.map(body => body.children.map(description => description.text.length !== 0 && console.log(description.text))))
+    // console.log(products.body)
   }
   useEffect(() => {
  fetchdata();
 
   },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+      <Route element={<Homepage />} path="/">
+            <Route path="/" element={<Homepage />} />
+            <Route path="/sneakers" element={<ProductsPage />} />
+      </Route>
+      </Routes>
+    </>
   );
 }
 
